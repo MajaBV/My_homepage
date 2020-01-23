8c08e0a2cd7ea683022a125ca90159b7958f0787
Maja Vukadin <maja.barisic5@gmail.com>
2020-01-23 17:21:31 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about_me")
+def about_me():
+    return render_template("about.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/fakebook")
+def fakebook():
+    return render_template("fakebook.html")
+
+
+if __name__ == '__main__':
+    app.run()
